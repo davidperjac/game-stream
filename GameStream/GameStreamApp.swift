714e7ba7fdc06c2c86d6406b1a5ec0e55ee54f85
davidperjac <diperez@espol.edu.ec>
2023-04-18 12:32:51 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by David Perez on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
